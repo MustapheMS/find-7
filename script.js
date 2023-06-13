ab3d61db1963e7141c375efbc70c1e439acb567f
MustapheMS <moztaphem9@gmail.com>
2023-06-13 21:54:37 +0300

    Find 7 Done

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Good Luck 💪🏾
+
+
+//Done 💯💯
+
+let findSeven = number => {
+    for (let i = 0; i < number.length; i++) {
+      if (number[i] === 7) {
+        return "FOUND";
+      }
+    }
+    return "NOT FOUND";
+  }
+  
+  console.log(findSeven([1, 2, 3, 4, 5, 6,7]));
